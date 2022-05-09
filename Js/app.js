ece68da0d752be13fc700ef0bb2f376ed827594c
Tfragao <fragaot@gmail.com>
2022-05-09 20:01:49 +0200

    Basic functionality: added to buttons to change language from English to Spanish

--- Js/app.js
+++ Js/app.js
@@ -11,6 +11,32 @@ function hideMenu(){
     navLink.style.right = "-200px";
  }
 
+ // Create a function to change the hash value of the page
+function changeLanguage(lang) {
+    location.hash = lang;
+    location.reload();
+}
+
+// language translations
+var language = {
+    en: {
+        welcome: "welocme in english"
+    },
+    es:{
+        welcome : "Bienvenido a todos"
+    }
+};
+
+// define language via window hash
+if(window.location.hash){
+    if(window.location.hash === "#es"){
+        welcomeSection.textContent = language.es.welcome;
+       
+    }    
+}
+
+
+
  
 
 
