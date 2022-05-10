0accd6011511bdf1d019e214dd400e975b4fc820
Tfragao <fragaot@gmail.com>
2022-05-10 13:29:03 +0200

    First attempt to translate the home page manually: It works, But I have to find a better way to automate it.

--- Js/app.js
+++ Js/app.js
@@ -20,17 +20,42 @@ function changeLanguage(lang) {
 // language translations
 var language = {
     en: {
-        welcome: "welocme in english"
+        // Just letting the default in English for now
+      
     },
     es:{
-        welcome : "Bienvenido a todos"
+        welcome : "Un lugar donde viven las mentes más brillantes.",
+        paragraph : `Conocimiento que vale la pena difundir, en la universidad de Eduford profundizará 
+                     sus conocimientos en una variedad de areas, este es el lugar para estar.`,
+        visit :   "Visítanos para saber más",
+        couseOffer : "Cursos que ofrecemos",
+        courseParagraph : `Con más de 3000 estudiantes universitarios y 1500 graduados estudiando en nuestra universidad,
+                           ofrecemos muchas oportunidades de estudio en todo el mundo`,
+        undergraduate : "licenciatura",
+        undergradTexto : `Los estudios de grado en Eduford son de alta calidad con un enfoquepráctico. 
+                          Se espera que los estudiantes cumplan con los criterios básicosmínimos en los primeros años 
+                          académicos y luego en los ultimos años empiezancon las praticas en empresas.`,
+        graduate : "Posgrado",
+        graduateTexto : `Los estudios de Posgrado en Eduford ofrecen recursos de desarrollo profesional, 
+                         oportunidades de creación de contactos entre varias universidades y  
+                         todo el apoyo para los estudiantes de Master, doctorado y títulos profesionales`
+
+
     }
 };
 
 // define language via window hash
 if(window.location.hash){
     if(window.location.hash === "#es"){
-        welcomeSection.textContent = language.es.welcome;
+        welcomeSectionh1.textContent = language.es.welcome;
+        welcomeSectP.textContent = language.es.paragraph;
+        visita.textContent = language.es.visit;
+        courseSecth1.textContent = language.es.couseOffer;
+        courseSectP.textContent = language.es.courseParagraph;
+        undergraduateHeader.textContent = language.es.undergraduate;
+        undergradTexto.textContent = language.es.undergradTexto;
+        graduateHeader.textContent = language.es.graduate;
+        graduateTexto.textContent = language.es.graduateTexto;
        
     }    
 }
